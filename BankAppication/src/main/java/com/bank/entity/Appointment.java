c22a5a66159b606d705f99a39aabe3d81c1af7f1
saumendra <saumendra08@gmail.com>
2020-03-13 08:17:19 +0530

    First entity

---
+++ BankAppication/src/main/java/com/bank/entity/Appointment.java
@@ -0,0 +1,89 @@
+package com.bank.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+@Entity
+@Table(name = "appointment")
+public class Appointment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "Id")
+	private Long id;
+	@Column(name = "Date")
+    private Date date;
+	@Column(name = "Location")
+    private String location;
+	@Column(name = "Descriprion")
+    private String description;
+	@Column(name = "Confirmed")
+    private boolean confirmed;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isConfirmed() {
+		return confirmed;
+	}
+
+	public void setConfirmed(boolean confirmed) {
+		this.confirmed = confirmed;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public String toString() {
+		return "Appointment [id=" + id + ", date=" + date + ", location=" + location + ", description=" + description
+				+ ", confirmed=" + confirmed + ", user=" + user + "]";
+	}
+
+	
+	
+}
